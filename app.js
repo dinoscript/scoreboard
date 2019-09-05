552c72c8718d5691a6caf31768b1894b8fdd3edb
Volodymyr Skoroplias (Osf-commerce) <volodymyr.skoroplias@osf-commerce.com>
2019-09-05 12:45:11 +0300

    iterating and rendering with loops

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+const players = [
+  {
+    name: "Guil",
+    score: 50
+  },
+  {
+    name: "Treasure",
+    score: 85
+  },
+  {
+    name: "Ashley",
+    score: 95
+  },
+  {
+    name: "James",
+    score: 80
+  }
+];
+
 const Header = (props) => {
   return (
     <header>
@@ -11,7 +30,7 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
-        {props.name}
+        { props.name }
       </span>
 
       <Counter score={props.score} />
@@ -29,24 +48,26 @@ const Counter = (props) => {
   );
 }
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="scoreboard">
       <Header 
         title="Scoreboard" 
-        totalPlayers={1} 
+        totalPlayers={props.initialPlayers.length} 
       />
 
       {/* Players list */}
-      <Player name="Guil" score={50} />
-      <Player name="Treasure" score={90} />
-      <Player name="Ashley" score={85} />
-      <Player name="James" score={80} />
+      {props.initialPlayers.map( player =>
+        <Player 
+          name={player.name}
+          score={player.score}            
+        />
+      )}
     </div>
   );
 }
 
 ReactDOM.render(
-  <App />,
+  <App initialPlayers={players} />,
   document.getElementById('root')
 );
