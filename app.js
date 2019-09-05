f7225d13d176ff5b16da80f3cfc77a3db1d582d9
Volodymyr Skoroplias (Osf-commerce) <volodymyr.skoroplias@osf-commerce.com>
2019-09-05 12:44:27 +0300

    using props to create reusable components

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ const Header = (props) => {
   );
 }
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
-        Guil
+        {props.name}
       </span>
 
-      <Counter />
+      <Counter score={props.score} />
     </div>
   );
 }
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{ props.score }</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
@@ -38,7 +38,10 @@ const App = () => {
       />
 
       {/* Players list */}
-      <Player />
+      <Player name="Guil" score={50} />
+      <Player name="Treasure" score={90} />
+      <Player name="Ashley" score={85} />
+      <Player name="James" score={80} />
     </div>
   );
 }
